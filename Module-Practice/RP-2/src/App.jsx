172a6284cbd4241f290cb83dc86be9abe7173c80
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-03-26 13:43:41 +0600

    React-Practice-2-Users-Commit

--- Module-Practice/RP-2/src/App.jsx
+++ Module-Practice/RP-2/src/App.jsx
@@ -1,6 +1,12 @@
+import { Suspense } from 'react'
 import './App.css'
 import Batsman from './Batsman'
 import Counter from './Counter'
+import Users from './Users'
+
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/posts')
+.then(res => res.json())
 
 function App() {
 
@@ -8,8 +14,11 @@ function App() {
     <>
       <h2>React + RP-2</h2>
 
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Users fetchUsers = {fetchUsers}></Users>
+      </Suspense>
       <Batsman></Batsman>
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
      
     </>
   )
---
+++ Module-Practice/RP-2/src/Summary.jsx
@@ -0,0 +1,26 @@
+/**
+ * Components
+ * JSX
+ * Props
+ * State
+ * Event
+ * Conditional Rendering
+ * API
+ */
+
+/**
+ * 1. just write a simple fetch with json conversion.
+ * 2. wrap the data loading component under suspense.
+ */
+
+
+// fetch ('https://jsonplaceholder.typicode.com/posts')
+// .then(response => response.json())
+// .then(data => console.log(data))
+
+
+// const loadData = async() => {
+//   const res = await fetch ('https://jsonplaceholder.typicode.com/posts');
+//   const data =  res.json();
+//   return data;
+// }
